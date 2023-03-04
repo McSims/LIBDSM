0aa60eecc1585ce056e36cab19a38296c92728d8
Maksims Puskels <maksims.puskels@gmail.com>
2023-03-04 14:22:37 +0200

    LIBSDM-0.0.1 with libdsm-0.3.2 + libtasn-4.12

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "LIBDSM",
                       url: "https://github.com/McSims/LIBDSM/releases/download/0.0.1/LIBDSM-0.0.1.xcframework.zip",
-                      checksum: "fa955714239057a4ad2079c5d37c845fa59116a67074e78cf84d1bb9084c04df")
+                      checksum: "589e1e094cd122c3010917f7dfad6d2af6e8c675453df929d06cfcc24967b61b")
     ]
 )
